c52f2777f53c1e0e832a637a94cd054eee56d3a4
lysanney <93539047+lysanney@users.noreply.github.com>
2022-04-01 15:53:51 +0200

    Added search bar with keyword alert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary.js";
+import "./Dictionary.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center">
+          <small>Coded by Lysanne Yap</small>
+        </footer>
+      </div>
     </div>
   );
 }
