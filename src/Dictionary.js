c3366b23b16b6cf69ed3d96b5dbdbbc2f20be0b4
lysanney <93539047+lysanney@users.noreply.github.com>
2022-04-03 19:22:51 +0200

    Rendered definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results.js";
 
 // API: https://dictionaryapi.dev/
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -26,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
